6aa6ff471230d09c4aa3b5c33df29177518a88ae
PurpleCharlie <pavel.samast@yandex.ru>
2025-08-08 06:57:52 +0300

    Add method BubbleSort algorithm

---
+++ Sorting-Algorithms-lesson-1/Extensions/NumericSortingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms_lesson_1.Extensions
+{
+    public static class NumericSortingExtensions
+    {
+        static void BubbleSort<T>(this T[] array)  where T : INumber<T>
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - i; j++)
+                { 
+                    if (array[j] > array[j + 1])
+                        (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                }
+            }
+        }
+    }
+}
