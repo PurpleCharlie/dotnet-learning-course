b0f93621c2efa669a1c264ccf792e1cc371a9c19
PurpleCharlie <pavel.samast@yandex.ru>
2025-08-11 08:46:59 +0300

    Add Models

---
+++ Factories-Units-Tanks-lesson-2/Models/Factory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories_Units_Tanks_lesson_2.Models;
+
+public class Factory
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ Factories-Units-Tanks-lesson-2/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factories_Units_Tanks_lesson_2.Models;
+
+public class Unit
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    /*          Foreign Key         */
+    public int FactoryId { get; set; }
+}
